fc3161e315d34bfc4c4d521768cb9c279a713173
victoriamuinde07 <victoriamuinde07@gmail.com>
2025-02-13 19:52:18 +0300

    included rest framework

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from datetime import timedelta 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,9 +41,21 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt'
     'corsheaders',
     'users',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : [
+      'rest_framework_simplejwt.authentication.JWTAuthentication',
+      ],
+}
+
+
+simple_JWT ={
+    'ACCESS_TOKEN_LIFETIME' : timedelta(minutes = 60),
+    'REFRESH_TOKEN_LIFETIME' : timedelta(days = 1),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
