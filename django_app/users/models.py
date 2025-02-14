6c00dc10cd450485e36a8344c96198b77db943a7
victoriamuinde07 <victoriamuinde07@gmail.com>
2025-02-14 10:28:05 +0300

    used signals to automatically create a profile when a user is created

--- django_app/users/models.py
+++ django_app/users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class CustomUser(AbstractUser):
@@ -26,5 +28,13 @@ class Profile(models.Model):
 
 def __str__(self):
     return f'{self.user.email}Profile'
+
+@receiver(post_save,sender=User)
+def create_user_profile(sender,instance,created,**kwargs):
+    if created:
+     Profile.objects.create(user=instance)
+@receiver(post_save,sender=User)
+def save_user_profile(sender,instance,**kwargs):
+    instance.profile.save()
 
 
--- django_app/users/urls.py
+++ django_app/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path 
 from .views import SignUpView
 from .views import SignInView
+from .views import ProfileView
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('signin/', SignInView.as_view(), name='signin'),
+    path('profile/',ProfileView.as_view(),name = 'profile'),
 ]
