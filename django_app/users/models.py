02f2019c5dfdd688f2a901c0cd8231e6d98b3ad7
Ron <114308701+roney-baraka@users.noreply.github.com>
2025-02-19 20:25:18 +0300

    Enhance create_user method to accept additional fields and improve email validation

--- django_app/users/models.py
+++ django_app/users/models.py
@@ -3,11 +3,12 @@ from django.db import models
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, password=None):
+    def create_user(self, email, username, password=None, **extra_fields):
         if not email:
-            raise ValueError("User must have an email address")
-        email = self.normalize_email
-        user = self.model(email=email, username=username)
+            raise ValueError("The Email field must be set")
+        
+        email = self.normalize_email(email)  
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
