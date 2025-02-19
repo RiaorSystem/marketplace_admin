6a6011cbfa5a35473e16cb5e5cb93c098f7109df
Ron <114308701+roney-baraka@users.noreply.github.com>
2025-02-19 12:10:46 +0300

    Add user profile management and role-based permissions
    - Introduced CustomUserManager for user creation and management.
    - Added profile picture and bio fields to CustomUser model.
    - Implemented UserRoles for role-based access control.
    - Created serializers for user registration, profile updates, and password changes.
    - Added ProfileView and ChangePasswordView for user profile management.
    - Updated requirements.txt to include new dependencies.

--- django_app/users/urls.py
+++ django_app/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from .views import SignUpView
-from .views import SignInView
+from .views import SignInView, SignUpView,ProfileView, ChangePasswordView
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('signin/', SignInView.as_view(), name='signin'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'), 
 ]
