8d0178f8643e0019df04e726e7539daa0dc8f3ef
Ron <114308701+roney-baraka@users.noreply.github.com>
2025-02-19 17:20:15 +0300

    Add social authentication support with Django Allauth

--- django_app/users/urls.py
+++ django_app/users/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path 
+from django.urls import path, include
 from .views import SignInView, SignUpView,ProfileView, ChangePasswordView
+
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('signin/', SignInView.as_view(), name='signin'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('change-password/', ChangePasswordView.as_view(), name='change-password'), 
+    path('google/', include('allauth.socialaccount.urls')), 
 ]
