f4f37539c4bd367e4efe716ff14d89f8c59fe26b
VICTOR MUUO <vctrmuthoka@gmail.com>
2025-03-13 12:45:32 +0300

    Added Files

--- django_app/users/urls.py
+++ django_app/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import SignInView, SignUpView,ProfileView, ChangePasswordView
+from .views import SignInView, SignUpView,ProfileView, ChangePasswordView, AdminUserListView, AdminUserDetailView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
@@ -7,4 +7,6 @@ urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile'),
     path('change-password/', ChangePasswordView.as_view(), name='change-password'), 
     path('google/', include('allauth.socialaccount.urls')), 
+    path("admin/users/", AdminUserListView.as_view(), name="admin_users"),
+    path("admin/users/<int:pk>/", AdminUserDetailView.as_view(), name="admin_user_detail"),
 ]
