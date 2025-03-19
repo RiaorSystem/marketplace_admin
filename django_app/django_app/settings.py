b50295f7b1a3ec3e53c6524dc9e2f9a202c9204a
Ron <114308701+roney-baraka@users.noreply.github.com>
2025-03-19 17:13:44 +0300

    Add DATABASE_URL support for dynamic database configuration

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -184,6 +184,10 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.config(default=DATABASE_URL)
+
 
 
 # Password validation
