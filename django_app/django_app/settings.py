b934a048917ca25760c30a734a27e7be46e62d39
victoriamuinde07 <victroiamuinde07@gmail.com>
2025-06-19 20:05:09 +0300

    changed settings.py

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -49,7 +49,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Application definition
 
 INSTALLED_APPS = [
-    'users',
+    'django_app.users',
     'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
