6c7e513454e4180ff4f948d9e777f861fa3b8d0d
victoriamuinde07 <victroiamuinde07@gmail.com>
2025-06-19 17:47:02 +0300

    added deploying files

--- django_app/django_app/deployment.py
+++ django_app/django_app/deployment.py
@@ -51,33 +51,4 @@ DATABASES = {
         conn_max_age=600
     )
 }
-           
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    },
-}
 
-
-
-ADMINS = [("CBI Analytics", "YOUREMAIL@EMAIL.com")]
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-DEFAULT_FROM_EMAIL = 'default from email'
